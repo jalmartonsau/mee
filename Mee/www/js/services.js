19d8dbece9e4b9132fb73cc5299a02ffb842aaec
Jalmar Tõnsau <jalmartonsau@outlook.com>
2016-05-18 21:55:58 +0300

    Merge with Rauno

--- Mee/www/js/services.js
+++ Mee/www/js/services.js
@@ -1,50 +1,27 @@
-angular.module('starter.services', [])
+angular.module('calculator.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+/**
+ * A simple example service that returns some data.
+ */
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+.factory('History', function () {
+    return {
+        all: function () {
+            var historyString = window.localStorage['calculations'];
+            if (historyString) {
+                return angular.fromJson(historyString);
+            }
+            return [];
+        },
+        save: function (calculations) {
+            window.localStorage['calculations'] = angular.toJson(calculations);
+        },
+        newCalculation: function (calculation, value) {
+            // Add a new project
+            return {
+                expression: calculation,
+                output: value
+            };
         }
-      }
-      return null;
     }
-  };
-});
+})
